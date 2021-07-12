5aef11101a8f3fff235554bfb87447dd9eaef1b3
temitech812 <86545012+temitech812@users.noreply.github.com>
2021-07-12 10:25:31 +0100

    Intial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+document.getElementById('higher').addEventListener('click', function myFunction(){
+    var value = parseInt(document.getElementById('value').innerHTML)
+    if(value >= 50){
+        var newValue = value
+        document.getElementById('value').innerHTML = newValue
+    }else{
+        var newValue = value + 1
+        document.getElementById('value').innerHTML = newValue
+    }
+})
+
+document.getElementById('lower').addEventListener('click', function myFunction(){
+    var value = parseInt(document.getElementById('value').innerHTML)
+    if(value <= 0){
+        var newValue = value
+        document.getElementById('value').innerHtml = newValue
+    }else{
+    var newValue = value - 1
+    document.getElementById('value').innerHTML = newValue
+    }
+})
